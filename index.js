01a6e8f01f9a8a20aafa2c87032c035d4cdb4b81
Kaique Jacinto <79776257+KaiqueJacinto@users.noreply.github.com>
2023-11-09 13:31:37 +0000

    endpoint "READ ALL"

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"]
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
@@ -8,5 +10,10 @@ app.get('/', function (req, res) {
 app.get('/oi', function (req, res) {
   res.send('Olá, tudo bem!')
 })
+
+app.get('/herois', function (req,res){
+  res.send(herois)
+})
+
 
 app.listen(3000)
